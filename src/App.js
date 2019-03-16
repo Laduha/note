afbafa2d789e0a4b9289548bdc775951793946ca
Laduha <cobra.sniper@mail.ru>
2019-03-16 16:38:45 +0200

    Minor changes, add Header component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Header from "./components/Header";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-        </header>
-      </div>
+      <React.Fragment>
+        <Header />
+      </React.Fragment>
     );
   }
 }
---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div>
+      <nav>
+          <ul>
+              <li>Main</li>
+              <li>Settings</li>
+              <li>Log out</li>
+          </ul>
+      </nav>
+    </div>
+  )
+}
+
+export default Header;
